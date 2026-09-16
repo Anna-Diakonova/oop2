990ee9997e98a3821965e41399ab41fbccaa0a56
Anna-Diakonova <adiakonova958@gmail.com>
2026-09-16 19:42:26 +0300

    Add project files.

---
+++ oop2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions; 
+
+class Program
+{
+    static bool ValidateLogin(string login)
+    {
+        string pattern = @"^[a-zA-Z][a-zA-Z0-9]{1,9}$";
+
+        Regex regex = new Regex(pattern);
+
+        return regex.IsMatch(login);
+    }
+
+    static void Main()
+    {
+        string[] testLogins = { "User1", "a1", "1user", "veryLongLogin123", "usr@1", "A" };
+
+        foreach (var login in testLogins)
+        {
+            Console.WriteLine($"Логін '{login}': {ValidateLogin(login)}");
+        }
+    }
+}
